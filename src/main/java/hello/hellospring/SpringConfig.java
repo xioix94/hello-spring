95ad7ca462aa180be9c5ceae3bf599d7fcaaad0c
Kyeongjin Kim <xioix94@norma.co.kr>
2023-12-27 18:00:51 +0900

    section 6 spring integration test end

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,19 @@
 package hello.hellospring;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
+  private final DataSource dataSource;
+
+  public SpringConfig(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
 
   @Bean
   public MemberService memberService() {
@@ -17,6 +22,7 @@ public class SpringConfig {
 
   @Bean
   public MemberRepository memberRepository() {
-    return new MemoryMemberRepository();
+    // return new MemoryMemberRepository();
+    return new JdbcMemberRepository(dataSource);
   }
 }
