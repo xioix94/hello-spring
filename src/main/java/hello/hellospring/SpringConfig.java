5aa788df3c55213dc0ff860e977155eb3fa168a7
xioix94 <xioix94@gmail.com>
2024-01-14 00:44:33 +0900

    스프링 핷í핵심 원리 - 기본편 End

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -6,27 +6,27 @@ import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
 
-  private EntityManager em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
-  @Autowired
-  public SpringConfig(EntityManager em) {
-    this.em = em;
-  }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
 
-  @Bean
-  public MemberService memberService() {
-    return new MemberService(memberRepository());
-  }
-
-  @Bean
-  public MemberRepository memberRepository() {
-    // return new MemoryMemberRepository();
-    // return new JdbcMemberRepository(dataSource);
-//    return new JdbcTemplateMemberRepository(dataSource);
-    return new JpaMemberRepository(em);
-  }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//         return new MemoryMemberRepository();
+//         return new JdbcMemberRepository(dataSource);
+//         return new JdbcTemplateMemberRepository(dataSource);
+//         return new JpaMemberRepository(em);
+//    }
 }
